28c22fc5c24e4013142b87163348ec7565607fca
agustincarriso <86750198+agustincarriso@users.noreply.github.com>
2026-03-05 12:16:27 -0300

    ft: add new colliders

--- assets/scene/entity-names.ts
+++ assets/scene/entity-names.ts
@@ -5,21 +5,11 @@
  * Object containing all entity names in the scene for autocomplete support.
  */
 export enum EntityNames {
-  Bubble = "Bubble",
-  Bubble_2 = "Bubble_2",
-  Bubble_3 = "Bubble_3",
-  Button = "Button",
-  Button2 = "Button2",
-  Button3 = "Button3",
-  ButtonQuick = "ButtonQuick",
-  ButtonTank = "ButtonTank",
   LobbyBase_glb = "LobbyBase.glb",
-  Text = "Text",
-  Text_2 = "Text_2",
-  Text_3 = "Text_3",
+  Wall02_glb = "Wall02.glb",
+  Wall02_glb_2 = "Wall02.glb_2",
+  Wall02_glb_3 = "Wall02.glb_3",
+  Wall02_glb_4 = "Wall02.glb_4",
   floor_glb = "floor.glb",
-  wall01_glb = "wall01.glb",
-  wall01_glb_2 = "wall01.glb_2",
   wall01_glb_3 = "wall01.glb_3",
-  wall01_glb_4 = "wall01.glb_4",
 } 
