4d3beefd32ac1af8798bf8b36ff38cbcf7db5333
Giorgio <65674128+Gaffomdz@users.noreply.github.com>
2026-03-17 09:28:20 -0300

    feat: add-new-color-to-floor

--- assets/scene/entity-names.ts
+++ assets/scene/entity-names.ts
@@ -5,11 +5,11 @@
  * Object containing all entity names in the scene for autocomplete support.
  */
 export enum EntityNames {
+  Floor02_glb = "Floor02.glb",
   LobbyBase_glb = "LobbyBase.glb",
   Wall02_glb = "Wall02.glb",
   Wall02_glb_2 = "Wall02.glb_2",
   Wall02_glb_3 = "Wall02.glb_3",
   Wall02_glb_4 = "Wall02.glb_4",
   floor_glb = "floor.glb",
-  wall01_glb_3 = "wall01.glb_3",
 } 
